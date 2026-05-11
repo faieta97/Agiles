437906f60bdd630e89a03c8b953b6522daedc530
faieta97 <faieta@tech-bi.com>
2026-05-11 20:58:30 -0300

    Test 3 - Fede - Separar por "," y sumar

--- string_calculator.py
+++ string_calculator.py
@@ -1,4 +1,11 @@
 def add(numbers):
     if numbers == "":
         return 0
+    
+    if "," in numbers:
+       
+        lista_numeros = numbers.split(",")
+        
+        return int(lista_numeros[0]) + int(lista_numeros[1])
+    
     return int(numbers)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -4,4 +4,9 @@ def test_empty_string_returns_zero():
     assert add("") == 0
 
 def test_single_number():
-    assert add("1") == 1   
+    assert add("1") == 1    
+
+def test_two_numbers_comma_separated():
+    assert add("1,2") == 3
+    assert add("4,2") == 6
+    
