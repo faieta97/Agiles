43afa5ebf249b3823ffd6d017703f5e9569053ac
faieta97 <faieta@tech-bi.com>
2026-05-11 21:30:35 -0300

    Test 4 - Benja - Suma de muchos numeros

--- string_calculator.py
+++ string_calculator.py
@@ -2,10 +2,4 @@ def add(numbers):
     if numbers == "":
         return 0
     
-    if "," in numbers:
-       
-        lista_numeros = numbers.split(",")
-        
-        return int(lista_numeros[0]) + int(lista_numeros[1])
-    
-    return int(numbers)
+    return sum(int(n) for n in numbers.split(","))
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,12 +1,7 @@
 from string_calculator import add
 
-def test_empty_string_returns_zero():
-    assert add("") == 0
+def test_multiple_numbers():
 
-def test_single_number():
-    assert add("1") == 1    
 
-def test_two_numbers_comma_separated():
-    assert add("1,2") == 3
-    assert add("4,2") == 6
-    
+    assert add("1,2,3") == 6
+    assert add("1,2,3,4,5") == 15
